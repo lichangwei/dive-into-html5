b2ad97e34ea55dedddf777af8cf4e372d5f625ed
lichangwei <lichangwei@love.com>
2013-08-29 19:42:38 +0800

    added cross domain sample

---
+++ cross-domain/iframe/utils.js
@@ -0,0 +1,8 @@
+
+var domain_a = 'http://domain.a.com';
+var domain_b = 'http://domain.b.com';
+var path_to_root = '/github/dive-into-html5/cross-domain/iframe';
+
+function getRandom(min, max){
+  return Math.floor(Math.random() * (max - min)) + min;
+}
